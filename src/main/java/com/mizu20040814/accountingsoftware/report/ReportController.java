13e15eecf38d99d4348c0908172ce2f74d502791
mizu20040814 <mizu20040814@gmail.com>
2026-02-21 12:54:48 +0900

    feat:Controller修正

--- src/main/java/com/mizu20040814/accountingsoftware/report/ReportController.java
+++ src/main/java/com/mizu20040814/accountingsoftware/report/ReportController.java
@@ -9,7 +9,7 @@ import java.time.LocalDate;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/report")
+@RequestMapping("/api/reports")
 public class ReportController {
 
     private final ReportService reportService;
